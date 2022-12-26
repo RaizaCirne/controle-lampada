4f9f6c8741f99537f92291d460daba3b1ae75aea
Raíza Cirne Braz <raizabraz93@gmail.com>
2022-12-26 16:02:33 -0300

    add desafio area da circunferencia
    Realizando desafio da área da circunferência.

---
+++ programacao-do-zero/fundamentos/inicio/desafio_area.js
@@ -0,0 +1,7 @@
+// Calcular a área de uma circunferência
+// PI * raio * raio 
+
+const PI = 3.141592;
+let raio = 10; 
+let area = PI * raio * raio; 
+console.log("O resultado da área é " + area + "m²" );
