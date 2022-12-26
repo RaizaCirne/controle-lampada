602d20adea68512abf7e53e808a51ffa8ae13c9f
Raíza Cirne Braz <raizabraz93@gmail.com>
2022-12-26 11:38:34 -0300

    add dados
    Uso de variáveis let

--- programacao-do-zero/fundamentos/inicio/dados.js
+++ programacao-do-zero/fundamentos/inicio/dados.js
@@ -1,8 +1,8 @@
 var nome = "Caneta";
 var quantidade = 10;
 var preco = 6.4;
-var imposto = 1.5;
-var precoFinal = preco + imposto;
+let imposto = 1.5;
+let precoFinal = preco + imposto;
 
 console.log(nome);
 console.log(quantidade);
