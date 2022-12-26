721878d733ea8253a35532a7f5aeeeb6913e3e35
Raíza Cirne Braz <raizabraz93@gmail.com>
2022-12-25 23:51:05 -0300

    Desafio sentenca e bloco
    Primeiro desafio lançado.

---
+++ programacao-do-zero/fundamentos/inicio/desafio_sentenca_bloco.js
@@ -0,0 +1,16 @@
+// Desafio: Exibir no console os números pares e os números ímpares de 1 a 10.
+{
+  console.log(1);
+  console.log(3);
+  console.log(5);
+  console.log(7);
+  console.log(9);
+}
+
+{
+  console.log(2);
+  console.log(4);
+  console.log(6);
+  console.log(8);
+  console.log(10);
+}
