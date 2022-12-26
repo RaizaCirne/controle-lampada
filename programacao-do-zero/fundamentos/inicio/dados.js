a1fc2bd935ff069f70e9f06c8e91c81746ccb5ac
Raíza Cirne Braz <raizabraz93@gmail.com>
2022-12-26 11:37:43 -0300

    Update dados
    Uso de variáveis var

--- programacao-do-zero/fundamentos/inicio/dados.js
+++ programacao-do-zero/fundamentos/inicio/dados.js
@@ -1,5 +1,15 @@
-console.log("Caneta");
-console.log(10);
-console.log(6.4);
-console.log(1.5);
-console.log(7.9);
+var nome = "Caneta";
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto;
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(preco + imposto);
+console.log(precoFinal);
+
+nome = "Caneta BIC";
+console.log(nome);
