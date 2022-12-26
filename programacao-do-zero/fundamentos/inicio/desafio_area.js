d6eeabcb585e65e7eb848dc41f74761c2e7818db
Raíza Cirne Braz <raizabraz93@gmail.com>
2022-12-26 16:05:15 -0300

    add correcao desafio
    Correção desafio da área da circunferência.

--- programacao-do-zero/fundamentos/inicio/desafio_area.js
+++ programacao-do-zero/fundamentos/inicio/desafio_area.js
@@ -2,6 +2,10 @@
 // PI * raio * raio 
 
 const PI = 3.141592;
-let raio = 10; 
-let area = PI * raio * raio; 
-console.log("O resultado da área é " + area + "m²" );
+const raio = 10; 
+let areaCirc = PI * raio * raio; 
+
+console.log("O valor da área é " + areaCirc + "m²." );
+
+areaCirc = Math.PI * raio * raio
+console.log("O valor da área é " + areaCirc + "m².");
