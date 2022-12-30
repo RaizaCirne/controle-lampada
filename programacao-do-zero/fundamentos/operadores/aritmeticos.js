e60ae2f1a65a1e3287dd9d2a8e2f43d9eae3961e
Raíza Cirne Braz <raizabraz93@gmail.com>
2022-12-30 19:45:40 -0300

    add pratica operadores aritmeticos

---
+++ programacao-do-zero/fundamentos/operadores/aritmeticos.js
@@ -0,0 +1,20 @@
+// valores liteais
+console.log(7 + 3);
+console.log(6 - 7);
+console.log(7 * 9);
+console.log(8 / 3);
+console.log(8 % 3);
+
+// usando variáveis 
+let a  = 8; 
+let b = 11; 
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+a = a + b * a / b; 
+console.log(a)
+
