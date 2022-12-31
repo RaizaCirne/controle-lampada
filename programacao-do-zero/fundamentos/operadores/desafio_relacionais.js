b20f23e39102d754519bb1d70715b378ba3e8c45
Raíza Cirne Braz <raizabraz93@gmail.com>
2022-12-31 16:30:56 -0300

    add desafio relacionais

---
+++ programacao-do-zero/fundamentos/operadores/desafio_relacionais.js
@@ -0,0 +1,16 @@
+let a = 1;
+let b = 2;
+
+// encontre 5 expressões que o resultado é TRUE
+console.log(a < b);
+console.log(b >= a);
+console.log(a <= b);
+console.log(a !== b);
+console.log(b !== a);
+
+// encontro 5 expressões que o resultado é FALSE
+console.log(a > b);
+console.log(b < a);
+console.log(a === b);
+console.log(b <= a);
+console.log(a >= b);
