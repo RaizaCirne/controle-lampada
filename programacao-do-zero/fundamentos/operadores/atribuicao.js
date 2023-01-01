a8a2ab21fb7b55fffb214c0c5bc37009b01c80bc
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-01 13:28:08 -0300

    add pratica operadores de atribuicao

---
+++ programacao-do-zero/fundamentos/operadores/atribuicao.js
@@ -0,0 +1,19 @@
+let x = 10; // atribuindo 10 à variavel x
+x = x + 6;
+x = x - 1;
+
+let y = 20; // atribuindo 20 à variavel y
+y += 10; // y = y + 10; (Atribuição aditiva)
+y -= 2; // y = y - 2;   (Atribuição Subtrativa)
+y *= 2; // y = y * 2;   (Atribuição Multiplicativa)
+y /= 4; // y = y / 4;   (Atribuição de divisão)
+y %= 3; // y = y % 3;   (Atribuição de módulo)
+
+console.log(x);
+console.log(y);
+
+let w = "texto"; // atribuindo a string 'texto' a variável w
+w += ", mais texto"; // concatenação
+w += "!!!"; // concatenação
+
+console.log(w);
