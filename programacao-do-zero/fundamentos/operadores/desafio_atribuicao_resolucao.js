a0fea170a1f9e2224c8dd939e18b49abdd9327c6
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-01 15:09:39 -0300

    add resolucao desafio operadores de atribuicao

---
+++ programacao-do-zero/fundamentos/operadores/desafio_atribuicao_resolucao.js
@@ -0,0 +1,7 @@
+let texto = "Lista de Aprovados\n";
+texto += "==================\n";
+texto += "1. Ana Silva\n";
+texto += "2. Pedro Albuquerque\n";
+texto += "3. Guilherme Pereira\n";
+texto += "4. Rebeca França";
+console.log(texto);
