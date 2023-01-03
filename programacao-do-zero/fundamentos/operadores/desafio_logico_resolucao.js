8e862d3e23e387fc309db2c23ff4f19e1dd406be
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-03 13:05:47 -0300

    add resolucao desafio operadores logicos

---
+++ programacao-do-zero/fundamentos/operadores/desafio_logico_resolucao.js
@@ -0,0 +1,24 @@
+// Duas expressões resultar em TRUE (Aritméticos/Relacionais/Lógicos)
+let verdadeira1 = 2 + 5 >= 7 && 8 > 2;
+let verdadeira2 = !(6 * 6 < 3);
+
+console.log(verdadeira1);
+console.log(verdadeira2);
+
+
+// Duas expressões resultar em FALSE (Aritméticos/Relacionais/Lógicos)
+let falso1 = 8 - 4 > 12 || false || 15 === '15';
+let falso2 = !(7 !== 7 || '3' !== 3);
+
+console.log(falso1);
+console.log(falso2);
+/* 
+Duas expressões resultar em FALSE (Aritméticos/Relacionais/Lógicos)
+MINHA RESPOSTA: 
+
+let falso1 = 10 / 2 >= 5 * 3 || 10 % 2 != 0;
+let falso2 = !(55 * 4 === 220) !== !(220 === 4 * 55); 
+
+console.log(falso1);
+console.log(falso2);
+*/
