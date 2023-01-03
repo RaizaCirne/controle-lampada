017c60bf535f5409159a5580716564ddc651ff8e
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-03 08:44:14 -0300

    atualizar pratica 5

--- programacao-do-zero/fundamentos/operadores/logicos_1.js
+++ programacao-do-zero/fundamentos/operadores/logicos_1.js
@@ -1,11 +1,11 @@
 let temDinheiro = false;
-let estaEnsolarado = true;
+let estaEnsolarado = false;
 let carroEstaNaGaragem = false;
 
-let resultadoE = "#1 - Vai pro shopping? ";
+let resultadoE = "#1 (AND) - Vai pro shopping? ";
 resultadoE += temDinheiro && estaEnsolarado;
 console.log(resultadoE);
 
-let resultadoOU = "#2 - Vai pro shopping? ";
-resultadoOU += estaEnsolarado || carroEstaNaGaragem; 
+let resultadoOU = "#2 (OR) - Vai pro shopping? ";
+resultadoOU += estaEnsolarado || carroEstaNaGaragem;
 console.log(resultadoOU);
