1017aaa75fa175f3eb601e33ef1e0e0ac883b328
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-03 08:31:52 -0300

    Update logico_1

--- programacao-do-zero/fundamentos/operadores/logicos_1.js
+++ programacao-do-zero/fundamentos/operadores/logicos_1.js
@@ -1,6 +1,7 @@
 let temDinheiro = true; 
-let estaEnsolarado = true; 
+let estaEnsolarado = false; 
 
 let resultadoE = 'Vai pro shopping? ';
 resultadoE += temDinheiro && estaEnsolarado;
 console.log(resultadoE);
+
