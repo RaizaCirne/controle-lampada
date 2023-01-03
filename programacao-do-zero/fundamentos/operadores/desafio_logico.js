9c946a72efb0f6e33a0979cee14a251b321cd641
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-03 12:05:51 -0300

    add desafio operadores logicos

---
+++ programacao-do-zero/fundamentos/operadores/desafio_logico.js
@@ -0,0 +1,24 @@
+// Duas expressões resultar em TRUE (Aritméticos/Relacionais/Lógicos)
+let verdadeira1 = 10 + 5;
+let verdadeira2 = 4 / 2;
+
+let a = 3 * 5; 
+verdadeira1 = verdadeira1 >= a; 
+
+let b = 1 + 1; 
+verdadeira2 = verdadeira2 === b; 
+
+console.log(verdadeira1);
+console.log(verdadeira2);
+
+//Duas expressões resultar em FALSE (Aritméticos/Relacionais/Lógicos)
+let falso1 = 10 + 5; 
+let falso2 = 4 / 2; 
+
+falso1 = falso1 !== a; 
+
+falso2 = falso2 < b; 
+
+console.log(falso1);
+console.log(falso2);
+
