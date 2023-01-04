83e44862f5272dbe67ed70d25528a89d8b47563a
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-04 13:56:58 -0300

    add operador aritmetico

--- programacao-do-zero/fundamentos/operadores/desafio_logico_resolucao.js
+++ programacao-do-zero/fundamentos/operadores/desafio_logico_resolucao.js
@@ -8,7 +8,7 @@ console.log(verdadeira2);
 
 // Duas expressões resultar em FALSE (Aritméticos/Relacionais/Lógicos)
 let falso1 = 8 - 4 > 12 || false || 15 === '15';
-let falso2 = !(7 !== 7 || '3' !== 3);
+let falso2 = !(7 !== 6 + 1 || '3' !== 3);
 
 console.log(falso1);
 console.log(falso2);
