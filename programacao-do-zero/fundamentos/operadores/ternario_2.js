a24a603ef03c1ad3e4b3545af144cdcf3aeb91ce
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-07 15:49:01 -0300

    add pratica ternario atualizar
    Exemplo de um operador ternário dentro do outro.

--- programacao-do-zero/fundamentos/operadores/ternario_2.js
+++ programacao-do-zero/fundamentos/operadores/ternario_2.js
@@ -1,3 +1,4 @@
-const hora = 11; 
-const saudacao = hora <= 11 ? "Bom dia" : "Boa tarde"; 
-console.log(saudacao)
+const hora = 23;
+const saudacao =
+  hora <= 11 ? "Bom dia" : (hora <= 17 ? "Boa tarde" : "Boa noite");
+console.log(saudacao);
