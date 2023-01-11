6fe5c22615ac546451c1d25e52c614638bfd57ef
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-11 13:45:26 -0300

    add pratica 1 if else const
    Adicionando uma constante ao código.

--- programacao-do-zero/fundamentos/controle/if_else.js
+++ programacao-do-zero/fundamentos/controle/if_else.js
@@ -1,9 +1,12 @@
-if (true) 
+const verdadeira = true; 
+const falsa = false;
+
+if (verdadeira) 
   console.log("Exec...");
 else 
   console.log("Não exec...");
 
-if (false)
+if (falsa)
   console.log("Não exec...");
 else
   console.log("Exec...");
