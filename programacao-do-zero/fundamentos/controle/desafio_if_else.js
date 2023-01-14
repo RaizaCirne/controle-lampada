010fd52a2f2a36bb614e16f041b65b204005d75b
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-13 21:40:50 -0300

    atualizar desafio if else

--- programacao-do-zero/fundamentos/controle/desafio_if_else.js
+++ programacao-do-zero/fundamentos/controle/desafio_if_else.js
@@ -1,12 +1,12 @@
 const a = 10;
-const b = 20;
+const b = 28;
 const operacao = "*"; // + = * / %
 
 let resultado;
 
-if (a > 2 * b) {
+if (a + b > 2 * b) {
   resultado = "Falso";
-} else if (a < 2 * b) {
+} else if (a - b < 2 * b) {
   resultado = "Verdadeiro";
 } else if (a == b) {
   resultado = "Falso";
