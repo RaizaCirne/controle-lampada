691131ef657a9bc4b1a74196e3dadeed51a399de
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-15 19:52:53 -0300

    add pratica 4 while

---
+++ programacao-do-zero/fundamentos/controle/while_4.js
@@ -0,0 +1,17 @@
+
+let controle = 1; // variável que vai controlar o laço. 
+
+// Enquanto variável de controle for <= 10 repete o laço. 
+while ( controle <= 10) {
+  console.log(controle); // exibe o valor da variável de controle.
+  controle++; // Icrementa + 1
+}
+
+console.log('Fim!')
+
+/* O que foi necessário pra ter quantidade determinada de repetições? 
+1 - Variável que ajuda a controlar o laço (let controle = 1;)
+2 - A expressão ( controle <= 10) começando por verdadeira e passando a ser falsa quando chega em 11
+3 - Lógica que modificasse o valor da variável de controle (controle++;) 
+
+declaraçãoo; expressão; incremento -> Qtde determinada! */
