c1e53d8f6716fe2545fbea2eaf4cec7c94309476
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-14 21:44:23 -0300

    add pratica 2 while

---
+++ programacao-do-zero/fundamentos/controle/while_2.js
@@ -0,0 +1,8 @@
+let quantidade = 0;
+
+// inderterminada de repetições
+while (Math.random() < 0.97) {
+  quantidade++; //incremento usando operador unário: Pra cada repetição no while ele vai acrescentar uma quantidade
+}
+
+console.log("Qtde: " + quantidade);
