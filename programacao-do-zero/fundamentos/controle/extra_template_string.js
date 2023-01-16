4303c14cdf005263f1d9b11212aff1cfe81ed343
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-16 12:02:21 -0300

    add pratica extra template string

---
+++ programacao-do-zero/fundamentos/controle/extra_template_string.js
@@ -0,0 +1,9 @@
+const i = 2;
+const j = 4;
+
+console.log("i = " + i + ", j = " + j);
+console.log("i = " + i + ", j = " + j);
+
+console.log(`i = ${i}, j = ${j}`); // resolvendo valor de variável 
+console.log(`1 + 2 = ${1 + 2}`); // resolvendo expressão 
+console.log(`Valor gerado foi: ${Math.random()}`); // chamando função 
