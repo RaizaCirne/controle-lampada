42f61b169b1278c43791053080c46190e7902f9d
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-17 15:33:22 -0300

    add desafio for resolucao

---
+++ programacao-do-zero/fundamentos/controle/desafio_for_resolucao.js
@@ -0,0 +1,18 @@
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+
+// let linha = '';
+
+// for (let i = 1; i <=6 ; i++) {
+//   linha += '#'; 
+//   console.log(linha)
+// }
+
+// Fazer o for sem usar os números
+for (let s = '#'; s!= '#######'; s+= '#') {
+  console.log(s);
+}
