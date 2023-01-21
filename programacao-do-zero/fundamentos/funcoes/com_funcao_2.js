39e80c42046cb42e0ee5b45c50b175bf15edfd73
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-21 11:54:50 -0300

    add pratica COM funcao

--- programacao-do-zero/fundamentos/funcoes/com_funcao_2.js
+++ programacao-do-zero/fundamentos/funcoes/com_funcao_2.js
@@ -2,22 +2,19 @@ const d1 = 3;
 const m1 = 7;
 const a1 = 2021;
 
-function exibirData1() {
-  console.log(`${d1}/${m1}/${a1}`);
-}
-
 const d2 = 23;
 const m2 = 12;
 const a2 = 2022;
 
-function exibirData2() {
-  console.log(`${d2}/${m2}/${a2}`);
+function exibirData(d, m, a) {
+  console.log(`${d}/${m}/${a}`);
 }
 
-exibirData1();
-exibirData1();
-exibirData1();
+// Exibição a partir de uma função que recebe dados como entrada:
+exibirData(d1, m1, a1); 
+exibirData(d1, m1, a1);
+exibirData(d1, m1, a1);
 
-exibirData2();
-exibirData2();
-exibirData2();
+exibirData(d2, m2, a2);
+exibirData(d2, m2, a2);
+exibirData(d2, m2, a2);
