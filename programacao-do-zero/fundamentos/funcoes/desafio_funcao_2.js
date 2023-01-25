2632543e5cc495a60ee7130fbe171061645ae26c
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-25 17:18:27 -0300

    add desafio funcao 2

---
+++ programacao-do-zero/fundamentos/funcoes/desafio_funcao_2.js
@@ -0,0 +1,7 @@
+//Criar uma função que vai receber um parâmetro (nota) e essa função vai retornar o conceito.
+//desafio switch
+
+// Parâmetro (Nota) -> Retorno (Conceito)
+// Conceito de A até F 
+// Se nao achar a partir da nota conceito, retornar nulo
+// Pode usar if, else e switch também. 
