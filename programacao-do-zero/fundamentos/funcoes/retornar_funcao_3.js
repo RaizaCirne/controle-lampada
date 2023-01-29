fb23e88e93fd6368994970ab0800afc23d61bf83
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-29 15:22:46 -0300

    add conceito currying

---
+++ programacao-do-zero/fundamentos/funcoes/retornar_funcao_3.js
@@ -0,0 +1,23 @@
+//Princípio de programação funcional: Currying
+
+// Uma função que recebe um ou mais parâmetros
+function somar1(a, b) {
+  return a + b;
+}
+
+console.log(somar1(5, 4));
+
+// Criando função que retorna outra função - passando argumentos de forma parcial
+function somar2(a) {
+  return function (b) {
+    return a + b;
+  };
+}
+
+console.log(somar2(5)(4));
+
+// Criando outra função baseada na função somar2 com o parãmetro a definido pra 10.
+const somarMais10 = somar2(10);
+console.log(somarMais10(5));
+console.log(somarMais10(15));
+
