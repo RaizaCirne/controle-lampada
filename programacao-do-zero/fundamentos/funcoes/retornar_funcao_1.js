ed8ede558f105a721df901991bdd8248eed6be7d
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-29 12:58:47 -0300

    add retornar funcao

---
+++ programacao-do-zero/fundamentos/funcoes/retornar_funcao_1.js
@@ -0,0 +1,14 @@
+function retornaUmaFuncao() {
+  function bomDia() {
+    return "Bom dia!!!";
+  }
+
+  return bomDia;
+}
+
+console.log(retornaUmaFuncao);
+console.log(retornaUmaFuncao());
+console.log(retornaUmaFuncao()());
+
+const umaFuncao = retornaUmaFuncao();
+console.log(umaFuncao);
