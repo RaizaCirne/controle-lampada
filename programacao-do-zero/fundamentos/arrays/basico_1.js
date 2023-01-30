b931c4f0173d74632ebba2a94cfa0a3f2fd2fdc4
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-30 13:48:24 -0300

    corrigir erro

--- programacao-do-zero/fundamentos/arrays/basico_1.js
+++ programacao-do-zero/fundamentos/arrays/basico_1.js
@@ -6,7 +6,7 @@ const notas = [7.8, 6.7, 10, 9.5, 7.8, 5.8];
 console.log(notas); // Exibe elementos do array
 console.log(notas[3]); // Acessa elemento de índice 3, que é a nota 9.5
 console.log(notas[5]); // Acessa elemento de índice 5, que é a nota 5.8
-console.log(notas[10]); // Elemento de índice 6 não faz parte do array, resultado é undefined.
+console.log(notas[10]); // Elemento de índice 10 não faz parte do array, resultado é undefined.
 
 // Alterando elementos do array
 notas[4] = 8.7; // altera nota do índice 4
