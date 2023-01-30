6f181509e8368c08c38d333ef6dd9eea7a8be70d
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-30 15:52:12 -0300

    add exemplo array vazio elementos

---
+++ programacao-do-zero/fundamentos/arrays/basico_2.js
@@ -0,0 +1,11 @@
+// Criando array vazio e colocando elementos depois 
+const nomes = []; // array vazio
+
+nomes[0] = "Pedro";
+nomes[1] = "Ana";
+nomes[2] = "Bia";
+nomes[3] = "Gui";
+nomes[4] = "Rebeca";
+nomes[9] = "Tiago"; // É o 10º elemento, pois começa a contar a partir do zero. 
+
+console.log(nomes);
