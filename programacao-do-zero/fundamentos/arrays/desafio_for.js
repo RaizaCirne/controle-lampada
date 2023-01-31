2ff061fd5f5b1b908ceff28df8dfdc8f2091e3a5
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-31 15:45:17 -0300

    add desafio for array

---
+++ programacao-do-zero/fundamentos/arrays/desafio_for.js
@@ -0,0 +1,8 @@
+// Percorrer elementos do array de forma decrescente
+// Percorrer o array começando a partir do penúltimo elemento, pulando de 2 em 2.
+// 9, 7, 5, 3, 1
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (i = 9; i < numeros.length && i > 0; i -= 2) {
+  console.log(i);
+}
