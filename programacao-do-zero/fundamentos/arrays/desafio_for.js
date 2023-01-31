9972fc5714969ae7d21fa4f4fa6d1085af7df66a
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-01-31 15:54:05 -0300

    corrigir

--- programacao-do-zero/fundamentos/arrays/desafio_for.js
+++ programacao-do-zero/fundamentos/arrays/desafio_for.js
@@ -3,6 +3,6 @@
 // 9, 7, 5, 3, 1
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for (i = 9; i < numeros.length && i > 0; i -= 2) {
+for (let i = 9; i < numeros.length && i > 0; i -= 2) {
   console.log(i);
 }
