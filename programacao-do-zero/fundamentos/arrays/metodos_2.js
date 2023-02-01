605f864e94921610517816617183276021fae617
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-02-01 20:36:39 -0300

    add comentarios metodos 2

--- programacao-do-zero/fundamentos/arrays/metodos_2.js
+++ programacao-do-zero/fundamentos/arrays/metodos_2.js
@@ -1,8 +1,17 @@
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8];
 
-numeros.splice(1, 2);
-console.log(numeros);
+// numeros.splice(1º param , 2º param) -> Remove elementos dentro do array. 
+// 1º parâmetro é o indice
+// 2º parâmetro é a quantidade de elementos que desejo excluir
 
-console.log(numeros.pop());
-console.log(numeros.pop());
-console.log(numeros);
+numeros.splice(1, 2); // Começando a partir do indice 1, excluindo 2 elementos 
+console.log(numeros); // [ 1, 4, 5, 6, 7, 8 ]
+
+// numeros.pop() -> Remove o último elemento do array 
+
+console.log(numeros.pop()); // Exclui o último elemento do array e mostra o que foi excluido -> 8 
+console.log(numeros.pop()); // Exclui mais um elemento do array e mostra o que foi excluido -> 7
+console.log(numeros); // Mostra os elementos alterados pelos métodos. 
+
+//OBS: métodos splice() e pop() causaram alteração no array
+// São metodos ou funções que pertecem a um objeto e geram impacto. 
