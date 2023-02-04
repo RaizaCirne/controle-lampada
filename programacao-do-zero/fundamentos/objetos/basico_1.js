df447a54a0570c495769a71d59fd8b8b0d171fbc
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-02-04 10:47:53 -0300

    add exemplo basico objetos

--- programacao-do-zero/fundamentos/objetos/basico_1.js
+++ programacao-do-zero/fundamentos/objetos/basico_1.js
@@ -1,4 +1,21 @@
-let x = {};
+const numeros = [10, 20, 30];
 
-//Verificando se é ou não um array
-console.log(Array.isArray(x));
+// 3 pares chave: valor, 
+const produto = {
+  nome: "Caneta",
+  preco: 9.99,
+  desconto: 0.2
+};
+
+console.log(numeros[0]); //Acessando elemento 10 dentro do array
+
+console.log(produto["nome"]); //Acessando o atributo nome dentro do objeto
+console.log(produto["preco"]); // //Acessando o atributo preço dentro do objeto
+console.log(produto["desconto"]); // //Acessando o atributo desconto dentro do objeto
+
+console.log(produto.nome); //Acessando atributo dentro do objeto através da notação ponto -> é mais usado
+console.log(produto.preco);  //Acessando atributo dentro do objeto através da notação ponto -> mais usado
+console.log(produto.desconto); 
+
+//Verificando o tipo da constante produto: tipo objeto
+console.log(typeof produto);
