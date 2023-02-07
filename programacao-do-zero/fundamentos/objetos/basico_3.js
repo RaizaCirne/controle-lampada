0dcadc54f630d13b1c158b17c50cba08fd27e231
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-02-07 11:31:54 -0300

    add alterar atributos dentro do objeto

--- programacao-do-zero/fundamentos/objetos/basico_3.js
+++ programacao-do-zero/fundamentos/objetos/basico_3.js
@@ -4,6 +4,11 @@ const data = {
   ano: 2021,
 };
 
+//Alterando valores/atributos dentro do objeto
+data.dia = 24;
+data.mes = 12; 
+
+
 // console.log(data.dia);
 // console.log(data.mes);
 // console.log(data.ano);
