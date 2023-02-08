97b0628e43adeca99d947fccce34026e3ebb34af
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-02-08 11:31:16 -0300

    add problema forma literal

---
+++ programacao-do-zero/fundamentos/objetos/problema_forma_literal.js
@@ -0,0 +1,34 @@
+// Forma literal: começa a ter códigos duplicados, muitas linhas de codigo pra definir o objeto
+// Exemplo: 
+
+const d1 = {
+  dia: 9,
+  mes: 10,
+  ano: 2010,
+  exibir: function () {
+    return `${this.dia}/${this.mes}/${this.ano}`;
+  },
+};
+
+const d2 = {
+  dia: 22,
+  mes: 11,
+  ano: 2023,
+  exibir: function () {
+    return `${this.dia}/${this.mes}/${this.ano}`;
+  },
+};
+
+const d3 = {
+  dia: 3,
+  mes: 4,
+  ano: 2022,
+  exibir: function () {
+    return `${this.dia}/${this.mes}/${this.ano}`;
+  },
+};
+
+console.log(d1.exibir());
+console.log(d2.exibir());
+console.log(d3.exibir());
+
