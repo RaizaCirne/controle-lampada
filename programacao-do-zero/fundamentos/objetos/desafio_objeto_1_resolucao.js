2af09b0d00ec642c9314f29b0137f3439192e1e2
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-02-08 11:09:31 -0300

    add desafio objeto 1 resolucao

---
+++ programacao-do-zero/fundamentos/objetos/desafio_objeto_1_resolucao.js
@@ -0,0 +1,12 @@
+const nascimento = {
+  dia: 9,
+  mes: 10,
+  ano: 2010,
+  exibir: function () {
+    return `${this.dia}/${this.mes}/${this.ano}`;
+  },
+};
+
+nascimento.dia = 11;
+
+console.log(nascimento.exibir());
