a00ebc153a85819e0bdabedc17b9b1869c4bc5e7
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-02-09 10:57:13 -0300

    add exemplo funcao e objeto 2

---
+++ programacao-do-zero/fundamentos/objetos/funcao_e_objeto_2.js
@@ -0,0 +1,18 @@
+function criarData(dia, mes, ano) {
+  return {
+    dia,
+    mes,
+    ano,
+    exibir() {
+      return `${this.dia}/${this.mes}/${this.ano}`;
+    },
+  };
+}
+
+const d1 = criarData(09, 10, 2010);
+const d2 = criarData(22, 11, 2023);
+const d3 = criarData(03, 04, 2022);
+
+console.log(d1.exibir());
+console.log(d2.exibir());
+console.log(d3.exibir());
