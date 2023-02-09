e5626fa3aeb2976127dc3704e380f3dcbb50ec2d
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-02-09 11:43:47 -0300

    add exemplo funcao construtora 2

--- programacao-do-zero/fundamentos/objetos/funcao_e_objeto_3.js
+++ programacao-do-zero/fundamentos/objetos/funcao_e_objeto_3.js
@@ -1,8 +1,26 @@
 // Função construtora (Classe) -> Objeto
-function Produto() {
-  console.log(this);
+function Data(dia = 1, mes = 1, ano = 2021) {
+  this.dia = dia;
+  this.mes = mes;
+  this.ano = ano;
+
+  this.exibir = function() {
+    return `${this.dia}/${this.mes}/${this.ano}`;
+  }
 }
 
-const p1 = new Produto();
+const d1 = new Data();
+d1.ano = 1970; 
+d1.mes = 12;
 
-console.log(typeof p1); 
+const d2 = new Data(24, 12, 2022);
+const d3 = new Data(31, 12, 2021);
+
+
+console.log(d1.exibir());
+console.log(d2.exibir());
+console.log(d3.exibir());
+
+console.log(d1);
+console.log(d2);
+console.log(d3);
