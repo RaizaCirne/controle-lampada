c5b3093e19218c5587b810ae8ed813c426f9fad8
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-02-11 20:38:23 -0300

    add resolucao ex02

---
+++ programacao-do-zero/fundamentos/exercicios/resolucao_ex02.js
@@ -0,0 +1,8 @@
+function converterIdadeEmAnosParaDias(idadeEmAnos) {
+  const diasDoAno = 365;
+
+  return diasDoAno * idadeEmAnos;
+}
+
+console.log(converterIdadeEmAnosParaDias(25));
+console.log(converterIdadeEmAnosParaDias(70));
