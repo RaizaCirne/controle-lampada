6df8014d18798c09336d358b1b3adc82cfea6b25
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-02-13 11:43:10 -0300

    add desafio ex04

--- programacao-do-zero/fundamentos/exercicios/ex04.js
+++ programacao-do-zero/fundamentos/exercicios/ex04.js
@@ -1,6 +1,44 @@
-//Crie uma função que recebe um número (de 1 a 12 e retorne o mês correspondente como uma  string. Por exemplo, se a entrada for 2, a função deverá retornar "fevereiro", pois este é o 2° mês.  
-// Exemplos:
+function receberNomeDoMes(nome) {
+  switch (nome) {
+    case 1:
+      return "Janeiro";
+    case 2:
+      return "Fevereiro";
+    case 3:
+      return "Março";
+    case 4:
+      return "Abril";
+    case 5:
+      return "Maio";
+    case 6:
+      return "Junho";
+    case 7:
+      return "Julho";
+    case 8:
+      return "Agosto";
+    case 9:
+      return "Setembro";
+    case 10:
+      return "Outubro";
+    case 11:
+      return "Novembro";
+    case 12:
+      return "Dezembro";
+    default:
+      return "Insira o valor correto";
+  }
+}
 
-nomeDoMes(1) // retornará "janeiro" 
-nomeDoMes(4) // retornará "abril"
-  
+console.log(receberNomeDoMes(1));
+console.log(receberNomeDoMes(2));
+console.log(receberNomeDoMes(3));
+console.log(receberNomeDoMes(4));
+console.log(receberNomeDoMes(5));
+console.log(receberNomeDoMes(6));
+console.log(receberNomeDoMes(7));
+console.log(receberNomeDoMes(8));
+console.log(receberNomeDoMes(9));
+console.log(receberNomeDoMes(10));
+console.log(receberNomeDoMes(11));
+console.log(receberNomeDoMes(12));
+console.log(receberNomeDoMes(13));
