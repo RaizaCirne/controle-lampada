841a318e75d0bf73500b974137a4a2f1e420d768
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-02-13 11:00:59 -0300

    add explicacao ex04

---
+++ programacao-do-zero/fundamentos/exercicios/ex04.js
@@ -0,0 +1,6 @@
+//Crie uma função que recebe um número (de 1 a 12 e retorne o mês correspondente como uma  string. Por exemplo, se a entrada for 2, a função deverá retornar "fevereiro", pois este é o 2° mês.  
+// Exemplos:
+
+nomeDoMes(1) // retornará "janeiro" 
+nomeDoMes(4) // retornará "abril"
+  
