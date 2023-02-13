97916d93b76ae92590b514ed5338922c0e10d1cf
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-02-12 21:38:18 -0300

    add novo ex03

--- programacao-do-zero/fundamentos/exercicios/ex03.js
+++ programacao-do-zero/fundamentos/exercicios/ex03.js
@@ -1,5 +1,14 @@
+// function calcularSalario(quantidadeHorasMes, ValorPorHora) {
+//   return quantidadeHorasMes * ValorPorHora;
+// }
+
+// console.log(`Salário é igual a R$ ${calcularSalario(150, 40.5)}`);
+
 function calcularSalario(quantidadeHorasMes, ValorPorHora) {
-  return quantidadeHorasMes * ValorPorHora;
+  const salarioBruto = quantidadeHorasMes * ValorPorHora;
+  const salarioLiquido = salarioBruto - (salarioBruto * 30) / 100;
+
+  return `Salário é igual a R$ ${salarioLiquido}`;
 }
 
-console.log(`Salário é igual a R$ ${calcularSalario(150, 40.5)}`);
+console.log(calcularSalario(180, 60));
