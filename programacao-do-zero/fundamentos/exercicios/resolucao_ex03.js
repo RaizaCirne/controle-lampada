b9aeaedd7b5e19584fa5e6fb9f1ad6ce82b8ce99
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-02-13 10:58:20 -0300

    add resolucao ex03

---
+++ programacao-do-zero/fundamentos/exercicios/resolucao_ex03.js
@@ -0,0 +1,7 @@
+function calcularSalarioLiquido(horasTrabalhadas, ganhoPorHora) {
+  const salarioBruto = horasTrabalhadas * ganhoPorHora;
+  const salarioLiquido = salarioBruto - (salarioBruto * 30) / 100;
+  return `Salário igual a R$ ${salarioLiquido}`;
+}
+
+console.log(calcularSalarioLiquido(180, 60));
