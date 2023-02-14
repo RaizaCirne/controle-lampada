9763db5ffc81f09d9d3e97c5a092a3d5e633f3d7
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-02-14 15:19:00 -0300

    add explicar ex05

---
+++ programacao-do-zero/fundamentos/exercicios/ex05.js
@@ -0,0 +1,5 @@
+//Crie uma função que receba dois números e retorne se o primeiro é maior ou igual ao segundo. Exemplos: 
+
+maiorOuIgual(0, 0) // retornará true 
+maiorOuIgual(0, "0") // retornará false 
+maiorOuIgual(5, 1) // retornará true
