e4629a6fa514e4c2187af14f72576e04483bd40b
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-02-23 18:15:18 -0300

    add descricao ex09

---
+++ programacao-do-zero/fundamentos/exercicios/ex09.js
@@ -0,0 +1,4 @@
+// Escreva uma função que receba dois parâmetros. O primeiro parâmetro é o elemento que repetirá, enquanto que o segundo será o número de vezes que haverá repetição. Um array será retornado. Exemplos:
+
+repetir("código", 2) // retornará ["código", "código"]
+repetir(7, 3) // retornará [7, 7, 7]
