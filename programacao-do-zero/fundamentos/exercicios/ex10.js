3ec643314cdc88929346ec0c05fad89b2dbb6e17
Raíza Cirne Braz <raizabraz93@gmail.com>
2023-02-28 20:56:40 -0300

    add comentario ex10

---
+++ programacao-do-zero/fundamentos/exercicios/ex10.js
@@ -0,0 +1,4 @@
+// Elabore uma função que recebe um número como parâmetro e retorne uma string com o símbolo "+" na quantidade especificada no parâmetro. Exemplos:
+
+simboloMais(2) // retornará "++"
+simboloMais(4) // retornará "++++
