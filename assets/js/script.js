4bd4175347016975f57bc54b9616b2a5e3574d4b
Raiza Cirne <raizacirne@gmail.com>
2024-02-28 17:11:41 -0300

    Feat: add proj

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,11 +17,16 @@ function off() {
   })
 }
 
-// Piscar lâmpada 
-const blinkLamp = document.getElementById('blink-blink')
+// Eventos de sincronização: permitem executar instruções ou funções em intervalos de tempo 
 function blink() {
-  blinkLamp.addEventListener('click', () => {
-    // add o que vai acontecer
-  })
-}
+  var intervalo = 0; // determinar a frequência com que a lâmpada vai piscar 
+  var contador = 0; // usar dentro da estrutura while 
 
+  // Enquando contador for menor que 10 a lâmpada vai piscar 10 vezes  
+  while(contador < 10) {
+    intervalo += 300; // intervalo é inicado com 0, += soma 300 ao 0. No próxim laço intervalo passa a valer 600. Soma 300 a cada looping 
+    setTimeout("document.getElementById('lamp').src='/assets/images/on.png';",intervalo); // método para trocar a imagem da lâmpada a cada intervalo. Intervalo é incremento a 300milisegundos a cada loop. 
+    setTimeout("document.getElementById('lamp').src='/assets/images/off.png';",intervalo);
+    contador++;
+  }
+}
