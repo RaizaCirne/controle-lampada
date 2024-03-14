d76b44aa0a4d237693148f975dc3c5cb78c5e364
Raiza Cirne <raizacirne@gmail.com>
2024-03-14 11:47:27 -0300

    refactor: método setTimeout

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,16 @@ function acender() {
 function apagar() {
   document.getElementById('lampada').src = 'assets/images/off.png'
 }
+
+// Evento de sincronização -> método setTimeout
+function piscar() {
+  let intervalo = 0; // determina a frequência que a lâmpada pisca
+  let contador = 0; // Incrementa a cada laço enquanto a condição for menor que 5 
+  while(contador < 5) {
+    intervalo += 250; // variável intervalo soma 250 a cada loop
+    setTimeout("document.getElementById('lampada').src = 'assets/images/on.png'", intervalo);
+    intervalo += 250; 
+    setTimeout("document.getElementById('lampada').src = 'assets/images/off.png'", intervalo);
+    contador++;
+  }
+}
